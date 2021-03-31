f61556aef66f0093b830a05a018dcb37fec13064
AnthonyLamour <a.lamour@ludus-academie.com>
2021-03-31 15:49:55 +0200

    Ajout de la page de suppression de client

--- PHP/SuppressionClients.php
+++ PHP/SuppressionClients.php
@@ -33,9 +33,177 @@
 
     <!--Contenu de la page-->
     <fieldset id="SuppressionClientContenu">
-        
+        <!--légende du Formulaire-->
+		<legend>
+            Formulaire de suppression
+        </legend>
+		<!--Formulaire de la page-->
+        <form id="MainFormulaire">
+			<!--label du select de client-->
+            <label for="SelectedClient">Choisissez un client :</label>
+			<!--select de client-->
+            <select id="SelectedClient">
+                <?php
+                    //connexion à la BDD grâce au fichier BDDconnect
+                    include 'BDDConnect.php';
+					//création de la requête sql permettant de récupérer les armes-->
+                    $sql = "SELECT * FROM CLIENT";
+					//pour chaque résultat
+                    foreach($conn->query($sql) as $Client){
+                        //ajout d'une option du select
+						echo '<option value = "'.$Client["ID"].'">'.$Client["NCLI"].' '.$Client["NOM"].'</option>';
+                    }
+                ?>
+            </select>
+			<!--bouton permettant de valider le Formulaire-->
+            <input type="button" id="Envoi" value="Valider" onclick="SupprimerUnClient()" />
+        </form>
     </fieldset>
+    <br/>
+    <!--Contenu de la page-->
+    <div id="ClientTableSansID">
+    
+    </div>
 
+    <script>
+        MainContent=document.getElementById("ClientTableSansID");
+        function SupprimerUnClient(){
+            //création du message de confirmation
+            var msg = 'Voulez vous vraiment supprimer le client :'+document.getElementById("SelectedClient").options[document.getElementById("SelectedClient").selectedIndex].text;
+            if (confirm(msg) ){
+                //reset du div de résultat
+                MainContent.innerHTML="";
+			    //création d'un nouveau tableau HTML
+                var newTable = document.createElement("table");
+                //convertion de l'objet JSON en chaine pour le passer en paramètre
+			    var dbParam = document.getElementById("SelectedClient").value;
+			    //création d'une requête XMLHttpRequest
+                var xhttp = new XMLHttpRequest();
+			    //lorsque la requête est envoyé
+                xhttp.onreadystatechange = function () {
+				    //si la requête est prête
+                    if (this.readyState == 4 && this.status == 200) {
+                        //création d'une nouvelle ligne du tableau
+                        var newLine = document.createElement("tr");
+                        //création d'une nouvelle colone du tableau
+                        var newCol = document.createElement("th");
+                        //set de l'attribut class de la case
+                        newCol.setAttribute("class", "TextCadre");
+                        //remplissage de la case
+                        newCol.innerHTML="Numéro  de client";
+                        //ajout de la case dans la ligne
+                        newLine.appendChild(newCol);
+                        //création d'une nouvelle colone du tableau
+                        var newCol = document.createElement("th");
+                        //set de l'attribut class de la case
+                        newCol.setAttribute("class", "TextCadre");
+                        //remplissage de la case
+                        newCol.innerHTML="Nom";
+                        //ajout de la case dans la ligne
+                        newLine.appendChild(newCol);
+                        //création d'une nouvelle colone du tableau
+                        var newCol = document.createElement("th");
+                        //set de l'attribut class de la case
+                        newCol.setAttribute("class", "TextCadre");
+                        //remplissage de la case
+                        newCol.innerHTML="Adresse";
+                        //ajout de la case dans la ligne
+                        newLine.appendChild(newCol);
+                        //création d'une nouvelle colone du tableau
+                        var newCol = document.createElement("th");
+                        //set de l'attribut class de la case
+                        newCol.setAttribute("class", "TextCadre");
+                        //remplissage de la case
+                        newCol.innerHTML="Localité";
+                        //ajout de la case dans la ligne
+                        newLine.appendChild(newCol);
+                        //création d'une nouvelle colone du tableau
+                        var newCol = document.createElement("th");
+                        //set de l'attribut class de la case
+                        newCol.setAttribute("class", "TextCadre");
+                        //remplissage de la case
+                        newCol.innerHTML="Catégorie";
+                        //ajout de la case dans la ligne
+                        newLine.appendChild(newCol);
+                        //création d'une nouvelle colone du tableau
+                        var newCol = document.createElement("th");
+                        //set de l'attribut class de la case
+                        newCol.setAttribute("class", "TextCadre");
+                        //remplissage de la case
+                        newCol.innerHTML="Compte";
+                        //ajout de la case dans la ligne
+                        newLine.appendChild(newCol);
+                        //ajout de la ligne dans le tableau
+                        newTable.appendChild(newLine);
+					    //récupération et parsage du résultat en JSON avec suppression du caractère vide correspondant à la validation de l'envoie
+                        var json = JSON.parse(this.responseText.substring(1));
+                        //pour chaque élément du JSON
+					    for (var i = 0; i < json.length; i++) {
+						    //création d'une nouvelle ligne du tableau
+                            newLine = document.createElement("tr");
+						    //création d'une nouvelle colone du tableau
+                            newCol = document.createElement("td");
+						    //set de l'attribut class de la case
+                            newCol.setAttribute("class", "TextCadre");
+						    //remplissage de la case
+                            newCol.innerHTML = json[i].NCLI;
+						    //ajout de la case dans la ligne
+                            newLine.appendChild(newCol);
+						    //création d'une nouvelle colone du tableau
+                            newCol = document.createElement("td");
+						    //set de l'attribut class de la case
+                            newCol.setAttribute("class", "TextCadre");
+						    //remplissage de la case
+                            newCol.innerHTML = json[i].NOM;
+						    //ajout de la case dans la ligne
+                            newLine.appendChild(newCol);
+						    //création d'une nouvelle colone du tableau
+                            newCol = document.createElement("td");
+						    //set de l'attribut class de la case
+                            newCol.setAttribute("class", "TextCadre");
+						    //remplissage de la case
+                            newCol.innerHTML = json[i].ADRESSE;
+						    //ajout de la case dans la ligne
+                            newLine.appendChild(newCol);
+						    //création d'une nouvelle colone du tableau
+                            newCol = document.createElement("td");
+						    //set de l'attribut class de la case
+                            newCol.setAttribute("class", "TextCadre");
+						    //remplissage de la case
+                            newCol.innerHTML = json[i].LOCALITE;
+						    //ajout de la case dans la ligne
+                            newLine.appendChild(newCol);
+						    //création d'une nouvelle colone du tableau
+                            newCol = document.createElement("td");
+						    //set de l'attribut class de la case
+                            newCol.setAttribute("class", "TextCadre");
+						    //remplissage de la case
+                            newCol.innerHTML = json[i].CATEGORIE;
+						    //ajout de la case dans la ligne
+                            newLine.appendChild(newCol);
+                            //création d'une nouvelle colone du tableau
+                            newCol = document.createElement("td");
+						    //set de l'attribut class de la case
+                            newCol.setAttribute("class", "TextCadre");
+						    //remplissage de la case
+                            newCol.innerHTML = json[i].COMPTE;
+						    //ajout de la case dans la ligne
+                            newLine.appendChild(newCol);
+						    //ajout de la ligne dans le tableau
+                            newTable.appendChild(newLine);
+                        }
+					    //ajout du tableau dans le div
+                        MainContent.appendChild(newTable);
+                    }
+                };
+                //ouverture du fichier XML
+                xhttp.open("GET", "SupprimerUnClient.php?id=" + dbParam, true);
+                //envoi de la requète
+                xhttp.send();
+            }
+        }
+    </script>
+    
     <!--footer-->
     <footer>
         <!--paragraphe de footer-->
